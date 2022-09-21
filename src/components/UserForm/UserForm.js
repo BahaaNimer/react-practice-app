dbd9a3b2fd9fd200743e2f5abf28bc84b943c028
bahaa nimer <bahaa@bahaa-nimer-pc.scoiumtech.local>
2022-09-21 15:01:18 +0300

    using useRef hook

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Button from '../UI/Button/Button';
 import Input from '../UI/Input/Input';
@@ -7,37 +7,33 @@ import Card from '../UI/Card/Card';
 import classes from './UserFrom.module.css'
 
 const UserForm = (props) => {
-  const [username, setUsername] = useState('');
-  const [age, setAge] = useState('');
+  const username = useRef();
+  const age = useRef();
+  // const [username, setUsername] = useState('');
+  // const [age, setAge] = useState('');
   const [error, setError] = useState();
 
   const userInfoHandler = (event) => {
     event.preventDefault();
-    if (username.trim().length === 0 || age.trim().length === 0) {
+    const enteredUsername = username.current.value;
+    const enteredAge = age.current.value;
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+age < 1 ) {
+    if (+enteredAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age greater than 0.',
       });
       return;
     }
-    props.onAddUser(username, age);
-    setUsername('');
-    setAge('');
-  };
-
-  const usernameHandler = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const ageHandler = (event) => {
-    setAge(event.target.value);
+    props.onAddUser(enteredUsername, enteredAge);
+    username.current.value = '';
+    age.current.value = '';
   };
 
   const errorHandler = () => {
@@ -45,7 +41,7 @@ const UserForm = (props) => {
   };
 
   return (
-    <div>
+    <>
       {error && (
         <ErrorModel
           title={error.title}
@@ -59,22 +55,20 @@ const UserForm = (props) => {
           <Input
             id="username"
             type="text"
-            value={username}
-            onChange={usernameHandler}
+            ref={username}
             placeholder="Enter your name"
           />
           <label htmlFor="age">Age (Years)</label>
           <Input
             id="age"
             type="number"
-            value={age}
-            onChange={ageHandler}
+            ref={age}
             placeholder='Enter your age'
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </>
   )
 }
 
